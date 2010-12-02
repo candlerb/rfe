489a581effff02ec585e053262c669fd75649b00
Brian Candler <B.Candler@pobox.com>
2010-12-02 20:03:30 +0000

    Better error for missing abstract_code chunk

--- src/rfe_cmd.erl
+++ src/rfe_cmd.erl
@@ -16,8 +16,13 @@ beam2erl() ->
 parse(beam) ->
   [Fname] = init:get_plain_arguments(),
   {ok, File} = file:read_file(Fname),
-  {ok, {Mod, [{abstract_code,{raw_abstract_v1,Forms}}]}} = beam_lib:chunks(File, [abstract_code]),
-  Forms;
+  Chunks = beam_lib:chunks(File, [abstract_code]),
+  case Chunks of
+  {ok, {Mod, [{abstract_code,{raw_abstract_v1,Forms}}]}}
+    -> Forms;
+  {ok,{rfe_parse,[{abstract_code,no_abstract_code}]}}
+    -> erlang:error(no_abstract_code)
+  end;
 
 parse(erl) ->
   [Fname] = init:get_plain_arguments(),

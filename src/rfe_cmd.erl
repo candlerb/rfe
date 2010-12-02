09dcfbf3eb6de1065f08a8a18727e3e80f57f8a1
Brian Candler <B.Candler@pobox.com>
2010-12-02 18:55:51 +0000

    Dump beam files

--- src/rfe_cmd.erl
+++ src/rfe_cmd.erl
@@ -1,15 +1,25 @@
 -module(rfe_cmd).
--export([erl2erl/0, erl2term/0]).
+-export([erl2erl/0, erl2term/0, beam2erl/0]).
 
 erl2term() ->
-  Forms = parsefile(),
-  io:format("% ~w forms~n~p", [length(Forms), Forms]).
+  Forms = parse(erl),
+  emit(term, Forms).
 
 erl2erl() ->
-  Forms = parsefile(),
-  lists:foreach(fun(Form) -> io:put_chars(erl_pp:form(Form)) end, Forms).
+  Forms = parse(erl),
+  emit(erl, Forms).
 
-parsefile() ->
+beam2erl() ->
+  Forms = parse(beam),
+  emit(erl, Forms).
+
+parse(beam) ->
+  [Fname] = init:get_plain_arguments(),
+  {ok, File} = file:read_file(Fname),
+  {ok, {Mod, [{abstract_code,{raw_abstract_v1,Forms}}]}} = beam_lib:chunks(File, [abstract_code]),
+  Forms;
+
+parse(erl) ->
   [Fname] = init:get_plain_arguments(),
   % {ok, File} = file:read_file(Fname),
   % {ok, Tokens, _} = erl_scan:string(binary_to_list(File)),
@@ -17,3 +27,8 @@ parsefile() ->
   {ok, Forms} = epp:parse_file(Fname, [], []),
   Forms.
 
+emit(term, Forms) ->
+  io:format("% ~w forms~n~p", [length(Forms), Forms]);
+
+emit(erl, Forms) ->
+  lists:foreach(fun(Form) -> io:put_chars(erl_pp:form(Form)) end, Forms).

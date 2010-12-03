d37d67565d317f69a69771b0343cd89750d2894d
Brian Candler <B.Candler@pobox.com>
2010-12-03 19:33:43 +0000

    Refactor all the translations into single command (rfet)

--- src/rfe_cmd.erl
+++ src/rfe_cmd.erl
@@ -1,12 +1,47 @@
 -module(rfe_cmd).
--export([convert/1]).
+-export([convert/0]).
 
-convert([From, To]) ->
-  Forms = parse(From),
-  emit(To, Forms).
+convert() ->
+  InputType = case init:get_argument(t) of
+    {ok, [[Flag]]} ->
+      list_to_existing_atom(Flag);
+    {ok, _} ->
+      erlang:error(conflicting_flag);
+    error ->
+      auto
+  end,
+  OutputType = case init:get_argument(b) of
+    {ok, [[Flag2]]} ->
+      list_to_existing_atom(Flag2);
+    {ok, _} ->
+      erlang:error(conflicting_flag);
+    error ->
+      erl  % default output type
+  end,
+  Files = init:get_plain_arguments(),
+  lists:foreach(fun(File) ->
+    convert(InputType, OutputType, File) end, Files).
 
-parse(beam) ->
-  [Fname] = init:get_plain_arguments(),
+convert(auto, OutputType, File) ->
+  InputType = case re:run(File, "\.[^.]+$", [{capture,all,list}]) of
+    {match, [Ext]} ->
+      io:write(Ext),
+      case Ext of
+        ".erl" -> erl;
+        ".hrl" -> erl;
+        ".beam" -> beam;
+        ".rfe" -> rfe
+      end;
+    _ ->
+      erlang:error(unknown_extension)
+  end,
+  convert(InputType, OutputType, File);
+
+convert(InputType, OutputType, File) ->
+  Forms = parse(InputType, File),
+  emit(OutputType, Forms).
+
+parse(beam, Fname) ->
   {ok, File} = file:read_file(Fname),
   Chunks = beam_lib:chunks(File, [abstract_code]),
   case Chunks of
@@ -16,16 +51,14 @@ parse(beam) ->
     -> erlang:error(no_abstract_code)
   end;
 
-parse(erl) ->
-  [Fname] = init:get_plain_arguments(),
+parse(erl, Fname) ->
   % {ok, File} = file:read_file(Fname),
   % {ok, Tokens, _} = erl_scan:string(binary_to_list(File)),
   % {ok, Form} = erl_parse:parse_form(Tokens),
   {ok, Forms} = epp:parse_file(Fname, [], []),
   Forms;
 
-parse(rfe) ->
-  [Fname] = init:get_plain_arguments(),
+parse(rfe, Fname) ->
   {ok, Forms} = rfepp:parse_file(Fname, [], []),
   Forms.
 
